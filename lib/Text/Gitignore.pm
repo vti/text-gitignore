9b6f77d40ab1935a0830c24a147aef47bcad999d
vti <viacheslav.t@gmail.com>
2020-01-14 18:37:58 +0100

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f06844c..64b3e5f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Text-Gitignore
    
     {{$NEXT}}
    
    +0.03 2020-01-14T17:37:54Z
    +
    +    - ack of trailing slash should not stop recursion by Eric A. Zarko
    +
     0.02 2019-03-27T20:54:45Z
    
         - fix negated patterns starting with / by Flavio Poletti

--- lib/Text/Gitignore.pm
+++ lib/Text/Gitignore.pm
@@ -4,7 +4,7 @@ use warnings;
 use base 'Exporter';
 
 our @EXPORT_OK = qw(match_gitignore build_gitignore_matcher);
-our $VERSION   = "0.02";
+our $VERSION   = "0.03";
 
 sub match_gitignore {
     my ( $patterns, @paths ) = @_;
